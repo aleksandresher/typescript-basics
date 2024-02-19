153981c396b94dc3b484f08b21285df7c1964822
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-02-20 01:13:32 +0400

    objects

--- build/js/main.js
+++ build/js/main.js
@@ -14,3 +14,32 @@ let myTuple = ["Dave", 42, true];
 myTuple[1] = 43;
 // myTuple[2] = 44; // Type 'number' is not assignable to type 'boolean'.
 ////////////////// Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj); // object
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Dave",
+    prop2: true,
+};
+// exampleObj.prop2 = 42; // Type 'number' is not assignable to type 'boolean'.
+exampleObj.prop1 = "John";
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5, "OU812"],
+};
+let jp = {
+    name: "Jimmy",
+    active: true,
+    albums: ["I", "II", "IV"],
+};
+//   evh.years = 40; // Property 'years' does not exist on type 'Guitarist'.
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name}`;
+    }
+    return "Hello!";
+};
+console.log(greetGuitarist(evh)); // Hello Eddie
--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,51 @@ myTuple[1] = 43;
 // myTuple[2] = 44; // Type 'number' is not assignable to type 'boolean'.
 
 ////////////////// Objects
+
+let myObj: object;
+myObj = [];
+console.log(typeof myObj); // object
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: "Dave",
+  prop2: true,
+};
+
+// exampleObj.prop2 = 42; // Type 'number' is not assignable to type 'boolean'.
+exampleObj.prop1 = "John";
+
+// type Guitarist = {
+//   name: string;
+//   active?: boolean;
+//   albums: (string | number)[];
+// };
+
+interface Guitarist {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+}
+
+let evh: Guitarist = {
+  name: "Eddie",
+  active: false,
+  albums: [1984, 5, "OU812"],
+};
+
+let jp: Guitarist = {
+  name: "Jimmy",
+  active: true,
+  albums: ["I", "II", "IV"],
+};
+
+//   evh.years = 40; // Property 'years' does not exist on type 'Guitarist'.
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) {
+    return `Hello ${guitarist.name}`;
+  }
+  return "Hello!";
+};
+console.log(greetGuitarist(evh)); // Hello Eddie
