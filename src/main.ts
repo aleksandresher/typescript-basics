d80d8c4872b68659f996add2df140fc27599db1c
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-02-21 21:44:34 +0400

    classes

--- src/main.ts
+++ src/main.ts
@@ -172,34 +172,149 @@
 
 //////////// Type assertion
 
-type One = string;
-type Two = string | number;
-type Three = "hello";
+// type One = string;
+// type Two = string | number;
+// type Three = "hello";
 
-/// convert to more or less specific
-let a: One = "hello";
-let b = a as Two; // less specific
-let c = a as Three; // more specific
+// /// convert to more or less specific
+// let a: One = "hello";
+// let b = a as Two; // less specific
+// let c = a as Three; // more specific
 
-let d = <One>"World";
-let e = <string | number>"world";
+// let d = <One>"World";
+// let e = <string | number>"world";
 
-const addOrConcat = (
-  a: number,
-  b: number,
-  c: "add" | "concat"
-): number | string => {
-  if (c === "add") return a + b;
-  return "" + a + b;
-};
+// const addOrConcat = (
+//   a: number,
+//   b: number,
+//   c: "add" | "concat"
+// ): number | string => {
+//   if (c === "add") return a + b;
+//   return "" + a + b;
+// };
 
-let myVal: string = addOrConcat(2, 2, "concat") as string;
+// let myVal: string = addOrConcat(2, 2, "concat") as string;
 
-/// be careful! TS sees no problem - but a string is returned
-let nextVal: number = addOrConcat(2, 2, "concat") as number;
+// /// be careful! TS sees no problem - but a string is returned
+// let nextVal: number = addOrConcat(2, 2, "concat") as number;
 
-//// The DOM
-const img = document.getElementById("img") as HTMLImageElement;
-const myImg = document.querySelector(".img")!;
+// //// The DOM
+// const img = document.getElementById("img") as HTMLImageElement;
+// const myImg = document.querySelector(".img")!;
 
-img.src;
+//////// Classes
+
+class Coder {
+  secondLand!: string;
+
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "Typescript"
+  ) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
+  public getAge() {
+    return `Hello, I'm ${this.age}`;
+  }
+}
+
+const Aleksandre = new Coder("Aleksandre", "Rock", 29);
+console.log(Aleksandre.getAge());
+
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
+  public getLang() {
+    return `I write ${this.lang}`;
+  }
+}
+
+const Sara = new WebDev("Mac", "Sara", "Lofi", 25);
+console.log(Sara.getLang());
+
+//////////////////////////////////////
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+  play(action: string) {
+    return `${this.name} ${action} the ${this.instrument}`;
+  }
+}
+
+const Page = new Guitarist("Jimmy", "guitar");
+console.log(Page.play("strums"));
+
+/////////////////////////////////////////
+
+class Peeps {
+  static count: number = 0;
+
+  static getCount(): number {
+    return Peeps.count;
+  }
+
+  public id: number;
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+
+console.log(John.id); // 1
+console.log(Peeps.count); // 3
+
+////////////////////////////
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+  public get data(): string[] {
+    return this.dataState;
+  }
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else throw new Error("Param is not an array of strings");
+  }
+}
+
+const MyBands = new Bands();
+MyBands.data = ["Neil Young", "Led Zep"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "ZZ Top"];
+console.log(MyBands.data);
+// MyBands.data = [123]; // Param is not an array of strings
+// at set data
